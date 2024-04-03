72861058d2c3fac1bec29b737c5886b5520dba72
auziqni <65333301+auziqni@users.noreply.github.com>
2024-04-03 08:28:17 +0700

    feat: mobile navbar

---
+++ components/helpscreen.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Helpscreen() {
+  return (
+    <main className="w-screen bg-blue-100 sm:bg-green-100 md:bg-yellow-100 lg:bg-orange-100 xl:bg-red-100 2xl:bg-slate-500">
+      help screen
+    </main>
+  );
+}

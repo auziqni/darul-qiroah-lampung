c44246a2354abe801949569336f3443ac555588e
auziqni <65333301+auziqni@users.noreply.github.com>
2024-04-08 16:29:27 +0700

    feat: about us mobile

--- components/landingPage/secAboutUs.tsx
+++ components/landingPage/secAboutUs.tsx
@@ -1,33 +1,70 @@
 import React from "react";
 import { SectionHead, SectionTag } from "@/components/landingPage/textStyling";
-import { Button } from "@/components/ui/button";
 import { ButtonPrimary } from "@/components/landingPage/interface";
+import Image from "next/image";
+import { twMerge } from "tailwind-merge";
 
 export default function SecAboutUs({ className }: { className?: string }) {
   return (
-    <div className={`${className} flex flex-col gap-3 p-10`}>
-      <div className=" flex flex-col gap-3 ">
-        <SectionTag>Tentang Yayasan Kami</SectionTag>
-        <SectionHead>
-          Kami Meperjuangkan Pendidikan, Kemanusiaan,dan Kebajikan
-        </SectionHead>
-      </div>
+    <div
+      id="SectionAboutUs"
+      className={twMerge(
+        ` flex flex-col gap-[30px] px-[15px] py-[40px]`,
+        className,
+      )}
+    >
+      <div id="heading" className="flex  flex-col gap-[30px]">
+        <div className=" flex flex-col gap-3 text-center  ">
+          <SectionTag>Tentang Yayasan Kami</SectionTag>
+          <SectionHead>
+            Kami Meperjuangkan Pendidikan, Kemanusiaan, Dan Kebajikan
+          </SectionHead>
+        </div>
 
-      <div>
-        <div>
-          <p>
-            Darul Qiroah Lampung adalah sebuah yayasan non-profit yang berfokus
-            pada pendidikan agama khususnya pembelajaran Al-Quran, serta sebagai
-            tempat penyalur donasi untuk mendukung program-program pendidikan
-            dan kegiatan sosial di Lampung.
+        <div className="flex flex-col gap-3 text-justify">
+          <p className=" ">
+            Selamat datang di{" "}
+            <span className="cursor-pointer text-green-600 hover:text-blue-800">
+              {" "}
+              Yayasan Darul Qiroah Lampung{" "}
+            </span>
+            , sebuah lembaga non-profit yang berkomitmen untuk meningkatkan
+            pendidikan agama khususnya dalam pembelajaran Al-Quran. Kami tidak
+            hanya sekadar menyediakan akses pendidikan berkualitas, tetapi juga
+            bertekad untuk menjadi pilar dalam memperkuat pemahaman agama di
+            masyarakat.
           </p>
           <p>
-            Bergabunglah bersama kami dalam misi suci ini. Mari kita
-            bersama-sama menjadikan pendidikan Al-Quran sebagai sumber
-            kebijaksanaan dan kebaikan.
+            Metode pembelajaran kami menitikberatkan pada pemahaman mendalam
+            terhadap Al-Quran serta pengaplikasian nilai-nilai Islam dalam
+            kehidupan sehari-hari. Kami juga menjadi tempat penyalur donasi,
+            memastikan setiap sumbangan digunakan secara transparan untuk
+            mendukung program-program pendidikan dan kegiatan sosial yang kami
+            jalankan. Bersama-sama, mari kita ciptakan perubahan yang positif
+            dan memberikan manfaat yang berkelanjutan bagi masyarakat Lampung
+            dan luar Lampung .
           </p>
         </div>
-        <ButtonPrimary>Donasi</ButtonPrimary>
+      </div>
+      <ButtonPrimary className="">Donasi</ButtonPrimary>
+
+      <div className="relative flex h-fit flex-row items-end justify-end border">
+        <div className="relative z-10 hidden h-[190px] w-[165px] translate-x-1/3  translate-y-1/3 overflow-clip rounded-bl-lg border-r-8 border-t-8 border-utama lg:block">
+          <Image
+            src={"/images/hero-image.jpg"}
+            alt="bg"
+            fill
+            objectFit="cover"
+          />
+        </div>
+        <div className=" relative h-64 w-full overflow-clip rounded-xl  md:h-[300px] md:w-[250px]">
+          <Image
+            src={"/images/hero-image.jpg"}
+            alt="bg"
+            fill
+            objectFit="cover"
+          />
+        </div>
       </div>
     </div>
   );

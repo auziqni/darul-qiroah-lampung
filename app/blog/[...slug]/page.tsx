604a3a6391c29dfda682390dd5eede35930a8fc6
auziqni <65333301+auziqni@users.noreply.github.com>
2024-04-17 01:18:36 +0700

    feat: blog

---
+++ app/blog/[...slug]/page.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { posts } from "#site/content";
+import { notFound } from "next/navigation";
+import { MDXContent } from "@/components/blog/mdx-components";
+import { Tag } from "@/components/blog/tag";
+import Image from "next/image";
+import fs from "fs";
+// import "@/styles/mdx.css";
+
+interface PostPageProps {
+  params: {
+    slug: string[];
+  };
+}
+
+async function getPostFromParams(params: PostPageProps["params"]) {
+  const slug = params?.slug?.join("/");
+  const post = posts.find((post) => post.slug === slug);
+
+  return post;
+}
+
+export default async function BlogPost({ params }: PostPageProps) {
+  const post = await getPostFromParams(params);
+  let image = post?.image;
+
+  fs.existsSync("public" + post?.image)
+    ? (image = post?.image)
+    : (image = "/images/placeholder.png");
+
+  image ? image : (image = "/images/placeholder.png");
+
+  if (!post) {
+    notFound();
+  }
+  return (
+    <article className="prose dark:prose-invert container mx-auto flex max-w-3xl flex-col py-6">
+      <div className="relative -z-10 mb-10 h-96 w-full">
+        <Image
+          src={image}
+          alt="bg"
+          fill
+          objectFit="cover"
+          objectPosition="center"
+        />
+      </div>
+      <h1 className="mb-2">{post.title}</h1>
+      <div className="mb-2 flex gap-2">
+        {post.tags?.map((tag) => <Tag tag={tag} key={tag} />)}
+      </div>
+      {post.description ? (
+        <p className="mt-0 text-xl text-muted-foreground">{post.description}</p>
+      ) : null}
+      <hr className="my-4" />
+      <div>
+        <MDXContent code={post.body} />
+      </div>
+      {/* {post.body} */}
+    </article>
+  );
+}
